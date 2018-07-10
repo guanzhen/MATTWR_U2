627538c0691a71253668e7ed4fcdd7dd21a5e1a8
guanzhen <guanzhen@gmail.com>
2018-07-10 16:30:44 +0800

    Count up design.

--- ResetModule.vhd
+++ ResetModule.vhd
@@ -2,6 +2,9 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
+LIBRARY work;
+USE work.constants.all;
+
 ENTITY RESETMODULE IS
 
 GENERIC (
@@ -24,78 +27,68 @@ END;
 
 ARCHITECTURE LOGIC OF RESETMODULE IS 
 SIGNAL sResetConfig: STD_LOGIC_VECTOR(DATAWIDTH-1 downto 0); -- holds reset configuration
-SIGNAL sResetPeriod: STD_LOGIC_VECTOR(DATAWIDTH-1 downto 0); -- number of counts in us.
+--SIGNAL sResetPeriod: STD_LOGIC_VECTOR(DATAWIDTH-1 downto 0); -- number of counts in us.
+SIGNAL sResetPeriod: integer range 0 to MAXCOUNT-1 := DEFAULT_RESET_PERIOD; -- number of counts in us.
 SIGNAL sEnable     : STD_LOGIC:= '0';
+SIGNAL sReset      : STD_LOGIC:= '0'; -- active high output when reset counter has elasped
 SIGNAL sCounter1USOF : STD_LOGIC:= '0'; -- signal for overflow output from counter2
+
 BEGIN
 
 oResetConfig <= sResetConfig;
-oResetPeriod <= sResetPeriod;
+oResetPeriod <= std_logic_vector(to_unsigned(sResetPeriod, oResetPeriod'length));
 sEnable <= sResetConfig(0); --bit 0 is Enable signal
+oReset <= sReset;
 -- Process for writing into module registers.
-WRITEREG : PROCESS(inReset,iWrConfig,iWrPeriod,iData,sCounter1USOF)
+WRITEREG : PROCESS(inReset,iWrConfig,iWrPeriod,iData,sReset)
 BEGIN  
-  if (inReset = '0' or sCounter1USOF = '1') then                        -- reset sResetConfig register to default values
+  if (inReset = '0' or sReset = '1') then                        -- reset sResetConfig register to default values
     sResetConfig <= (others =>'0');
   elsif rising_edge(iWrConfig) then              -- write to config register 
       sResetConfig <= iData;
   end if;
   
   if (inReset = '0') then  -- reset sResetPeriod register to default values
-    sResetPeriod <= (others =>'0');
+    sResetPeriod <= DEFAULT_RESET_PERIOD;
   elsif rising_edge(iWrPeriod) then               -- write to period register 
-      sResetPeriod <= iData;
+    sResetPeriod <= to_integer(unsigned(iData(DATAWIDTH-1 downto 0)));
   end if;
 END PROCESS;
 
-
-COUNTER : PROCESS(inReset,sCounter1USOF,iWrPeriod,sResetPeriod)
-variable vCounter : integer range 0 to MAXCOUNT-1;
-variable vPeriod  : integer range 0 to MAXCOUNT-1;
+COUNTER : PROCESS(inReset,iCLK,sCounter1USOF,iWrPeriod,sResetPeriod,sEnable)
+variable vCounter : integer range 0 to MAXCOUNT-1:= 0;
 BEGIN
   if (inReset = '0' or iWrPeriod = '1') then -- reset internal counter on reset or write to period counter
-    vCounter := 0;    
-  elsif rising_edge(sCounter1USOF) then               -- increment counter
-    if sEnable = '1' then
+    vCounter := 0;
+    sReset <= '0';
+  else
+    if rising_edge(sCounter1USOF) and sEnable = '1' then               -- increment counter
       vCounter := vCounter + 1;
     end if;    
-  end if;
-  
-  -- set output reset signal
-  if inReset = '0' then
-    oReset <= '0';
-  else
-	 vPeriod := to_integer(unsigned(sResetPeriod(DATAWIDTH-1 downto 0)));
-    if vCounter = vPeriod then --assert reset signal when the values matches.
-      oReset <= '1';
-    else
-      oReset <= '0';
+     -- set output reset signal
+    if rising_edge(iCLK) and (vCounter = sResetPeriod) then
+      sReset <= '1';
     end if;
   end if;
-
 END PROCESS;
 
-COUNTER1US : PROCESS(iCLK,inReset,iWrPeriod)
-variable vCounter1US : natural range 0 to COUNTSPERUS-1;
+COUNTER1US : PROCESS(iCLK,inReset,iWrPeriod,sEnable)
+variable vCounter1US : natural range 0 to COUNTSPERUS;
 BEGIN
   if (inReset = '0' or iWrPeriod = '1') then -- reset internal counter on reset or write to period counter
     vCounter1US := 0;
+    sCounter1USOF <= '0';
   elsif rising_edge(iCLK) then
     if sEnable = '1' then
       vCounter1US := vCounter1US + 1;
-    end if; 
-  end if;
-  
+    end if;     
   --setoverflow signal when counter rolls over
-  if (inReset = '0' or iWrPeriod = '1') then
-    sCounter1USOF <= '0';
-  elsif rising_edge(iCLK) then
-    if vCounter1US = COUNTSPERUS-1 then
-      sCounter1USOF <= '1'; 
+    if vCounter1US = COUNTSPERUS then
+      sCounter1USOF <= '1';
+      vCounter1US := 0;
     else
       sCounter1USOF <= '0'; 
     end if;
-  end if;
-  
+  end if;  
 END PROCESS;
 END ARCHITECTURE LOGIC;
--- constants.vhd
+++ constants.vhd
@@ -4,5 +4,5 @@ USE ieee.std_logic_1164.all;
 package constants is
 
 constant NUM_OF_PWMMOD  : natural :=3;
-
+constant DEFAULT_RESET_PERIOD : integer := 1000; -- 1ms;
 end package constants;
