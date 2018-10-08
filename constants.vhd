6a20891528af1122560bb719130ae01dea938740
guanzhen <guanzhen@gmail.com>
2018-10-08 17:48:15 +0800

    Include defines for both Main and Sub Board

--- constants.vhd
+++ constants.vhd
@@ -3,14 +3,19 @@ USE ieee.std_logic_1164.all;
 
 package constants is
 
-constant VERSION : std_logic_vector(15 downto 0) := X"0001"; -- V0.01
+constant VERSION : std_logic_vector(15 downto 0) := X"0002"; -- V0.02
 constant NUM_OF_PWMMOD  : natural :=3;
 constant DEFAULT_RESET_PERIOD : integer := 1000; -- 1ms;
 constant ENC_WIDTH : natural := 32; -- 1ms;
 constant NUM_OF_INPUTS : natural := 22;
-constant NUM_OF_SE_INPUTS : natural := 13;
-constant NUM_OF_DIFFINPUTS : natural := 3;
 constant NUM_OF_OUTPUTS : natural := 16;
 constant DEFAULT_PWM_PERIOD : integer := 1000;
 constant DEFAULT_PWM_DUTY : integer := 100;
+
+-- Main Board
+constant NUM_OF_SE_INPUTS : natural := 14;
+constant NUM_OF_DIFFINPUTS : natural := 3;
+-- Sub Board / AIV Board
+--constant NUM_OF_SE_INPUTS : natural := 12;
+--constant NUM_OF_DIFFINPUTS : natural := 4;
 end package constants;
